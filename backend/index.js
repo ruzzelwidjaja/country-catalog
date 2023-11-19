924add28880fe8c7f6bc52062fb85e41c9b47033
ruzzel <ruzzelwidjaja@gmail.com>
2023-11-19 18:22:12 +0000

    chore(env): integrated environment variables for frontend and backend

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 const app = express();
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
+const port = process.env.PORT;
+console.log('TEST', process.env)
 const cors = require('cors');
 
 app.use(cors());
--- frontend/src/lib/cache.ts
+++ frontend/src/lib/cache.ts
@@ -1,8 +1,12 @@
 import { CountryDetailsType } from "@/lib/types";
 
+if (!import.meta.env.VITE_API_URL) {
+  throw new Error('API URL is not defined in environment variables');
+}
+
 interface CacheType {
   [key: string]: CountryDetailsType;
-}
+} 
 
 export const countryCache: CacheType = {};
 
@@ -13,14 +17,15 @@ export const fetchCountryData = async (countryName: string): Promise<CountryDeta
   }
 
   // Fetch new data
-  const response = await fetch(`http://localhost:3001/country/${countryName}`);
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/country/${countryName}`);
+  // const response = await fetch(`http://localhost:3001/country/${countryName}`);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
 
   const data: CountryDetailsType[] = await response.json();
   const countryData = data[0]; // Assuming the API returns an array and you need the first item
-
+  
   // Cache the data
   countryCache[countryName] = countryData;
 
