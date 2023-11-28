1181afa15de1a1092211a92a32b146c39e177589
ruzzel <ruzzelwidjaja@gmail.com>
2023-11-28 19:10:31 +0000

    fix: testing Vercel Analytics in main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,12 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import React from 'react'
+// import ReactDOM from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
  
 
+
+// ReactDOM.createRoot(document.getElementById('root')!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { inject } from '@vercel/analytics';
+import App from './App';
+import './index.css';
+
+// Vercel Analytics configuration
+inject({
+  mode: import.meta.env.PROD ? 'production' : 'development',
+  debug: !import.meta.env.PROD,
+  beforeSend: (event) => {
+    if (event.url.includes('/private')) {
+      return null;
+    }
+    return event;
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
